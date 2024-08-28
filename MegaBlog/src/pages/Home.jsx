d9303b59ba0bde265cd55187259871f174a9661c
Tanya <tannusharma.dk@gmail.com>
2024-08-28 22:04:40 +0530

    postcard responsive

--- MegaBlog/src/pages/Home.jsx
+++ MegaBlog/src/pages/Home.jsx
@@ -34,7 +34,7 @@ function Home() {
             <Container>
                 <div className='flex flex-wrap'>
                     {posts.map((post)=>(
-                        <div key={post.$id} className='p-2 w-1/4'>
+                        <div key={post.$id} className='p-2 sm:w-1/2 md:w-1/4'>
                             <PostCard {...post} />
                         </div>
                     ))}
